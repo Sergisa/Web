b02fd8d7b747783ff3fe6caa4264c5e4b5195eb4
Sergisa <phebra@yandex.ru>
2022-05-27 15:49:41 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+function chowModal(){
+    document.getElementById("modalOverlay1").classList.add('show')
+}
+function closeModal(){
+    document.getElementById("modalOverlay1").classList.remove('show')
+}
